bd41f3706c6866b648a3310e4fee67663cc34197
nejnej25 <perryjamesbelleza@gmail.com>
2018-07-28 16:09:36 +0800

    version 1

---
+++ email_attachment.pl
@@ -0,0 +1,33 @@
+#!/usr/bin/perl
+#
+use strict;
+use warnings;
+
+#Module for email with attachments
+use MIME::Lite;
+
+#Create the email
+my $mesg=MIME::Lite->new(
+		From=>'root@sniper1a.localdomain',
+		To=>$ARGV[0],
+		Type=>'multipart/mixed',
+		Subject=>$ARGV[1]
+	);
+
+#Create the body of the email
+$mesg->attach(
+	Type=>'TEXT',
+	Data=>"Hello this is mime lite"
+	);
+
+#Attachment of the email, in this sample
+#its image
+$mesg->attach(
+	Type=>'AUTO',
+	Path=>$ARGV[2],
+	Filename=>'image.jpg',
+	Disposition=>'attachment'
+	);
+
+#Send the email
+$mesg->send;
